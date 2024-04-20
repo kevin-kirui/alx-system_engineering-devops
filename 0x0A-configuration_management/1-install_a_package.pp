a7785098f8e68389c6a5ae2685d5b5667ec95126
kevin kirui <kipkoechkevin42@gmail.com>
2024-04-20 13:41:54 -0700

    modified file 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,9 @@
 #!/usr/bin/puppet
 
-# Install a specific version of flask (2.1.0)
+# Install a specific version of Flask (2.1.0) using Puppet
 
 package { 'flask':
-
     ensure   => '2.1.0',
-
     provider => 'pip'
-
 }
 
